be006967b981018afccb942e8d45b4009f6215df
qq554433768 <554433768@qq.com>
2018-11-02 18:47:05 +0800

    生产者service实现层

--- springcloud-eureka-client/src/main/java/com/jk/service/Coupon/CouponServiceImpl.java
+++ springcloud-eureka-client/src/main/java/com/jk/service/Coupon/CouponServiceImpl.java
@@ -4,6 +4,9 @@ package com.jk.service.Coupon;
 
 import com.jk.mapper.CouponMapper;
 import com.jk.model.Coupon;
+import com.jk.model.Order;
+import com.jk.model.OrderItem;
+import com.jk.model.OrderShipping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -44,4 +47,51 @@ public class CouponServiceImpl implements CouponServiceApi{
     public void editCoupon(Coupon coupon) {
         couponMapper.editCoupon(coupon);
     }
+
+    @Override
+    public List<Order> queryOrderList(Order order, Integer start, Integer pageSize) {
+        return couponMapper.queryOrderList(order,(start-1)*pageSize,pageSize);
+    }
+
+    @Override
+    public long queryOrderCount(Order order) {
+        return couponMapper.queryOrderCount(order);
+    }
+
+    @Override
+    public Order queryAllOrderById(String orderId) {
+        return couponMapper.queryAllOrderById(orderId);
+    }
+
+    @Override
+    public void editOrder(Order order) {
+        couponMapper.editOrder(order);
+    }
+
+    @Override
+    public List<OrderItem> getOrdersShow() {
+        return couponMapper.getOrdersShow();
+    }
+
+    @Override
+    public List<Order> getOrdersShow1() {
+        return couponMapper.getOrdersShow1();
+    }
+
+    @Override
+    public List<OrderShipping> getOrdersShow2() {
+        return couponMapper.getOrdersShow2();
+    }
+
+    @Override
+    public Order queryTuikUAn(String orderId) {
+        return couponMapper.queryTuikUAn(orderId);
+    }
+
+    @Override
+    public void editOrder666(Order order) {
+        couponMapper.editOrder666(order);
+    }
+
+
 }
