de1b558604538c360f5d2df7c935c3f9e2e1f707
qq554433768 <554433768@qq.com>
2018-11-02 18:36:04 +0800

    serviceApi新增订单

--- service-api/src/main/java/com/jk/service/Coupon/CouponServiceApi.java
+++ service-api/src/main/java/com/jk/service/Coupon/CouponServiceApi.java
@@ -1,7 +1,6 @@
 package com.jk.service.Coupon;
 
-import com.jk.model.Coupon;
-import com.jk.model.PetType;
+import com.jk.model.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,5 +23,22 @@ public interface CouponServiceApi {
     Coupon queryAllCouponById(@RequestParam("id")String id);
     @RequestMapping(value = "yx/editCoupon",method = RequestMethod.GET)
     void editCoupon(@RequestBody Coupon coupon);
-
+    @RequestMapping(value = "yx/queryOrderList",method = RequestMethod.GET)
+    List<Order> queryOrderList(@RequestBody Order order, @RequestParam("start")Integer start, @RequestParam("limit")Integer limit);
+    @RequestMapping(value = "yx/queryOrderCount",method = RequestMethod.GET)
+    long queryOrderCount(@RequestBody Order order);
+    @RequestMapping(value = "yx/queryAllOrderById",method = RequestMethod.GET)
+    Order queryAllOrderById(@RequestParam("orderId")String orderId);
+    @RequestMapping(value = "yx/editOrder",method = RequestMethod.GET)
+    void editOrder(@RequestBody Order order);
+    @RequestMapping(value = "yx/getOrdersShow",method = RequestMethod.GET)
+    List<OrderItem> getOrdersShow();
+    @RequestMapping(value = "yx/getOrdersShow1",method = RequestMethod.GET)
+    List<Order> getOrdersShow1();
+    @RequestMapping(value = "yx/getOrdersShow2",method = RequestMethod.GET)
+    List<OrderShipping> getOrdersShow2();
+    @RequestMapping(value = "yx/queryTuikUAn",method = RequestMethod.GET)
+    Order queryTuikUAn(@RequestParam("orderId")String orderId);
+    @RequestMapping(value = "yx/editOrder666",method = RequestMethod.GET)
+    void editOrder666(@RequestBody Order order);
 }
